2244b9721ee7bac497bdadf78294a5af494bff91
Raghu Gudla <raghu3786@gmail.com>
2019-02-21 16:52:06 +0100

    initial setup

---
+++ src/main/java/com/github/raghugudla/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -0,0 +1,47 @@
+package com.github.raghugudla.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    private static Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+    public static void main(String[] args) {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for(int i=0; i<10; i++) {
+            //producer record
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world from java-"+i);
+
+            //send data
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if(e == null) {
+                        logger.info("Received new metadata. Topic: " + recordMetadata.topic() + ", Timestamp: " + recordMetadata.timestamp());
+                    }
+                    else {
+                        logger.info("err " + e);
+                    }
+                }
+            });
+        }
+
+        producer.flush();
+        producer.close();
+    }
+}
