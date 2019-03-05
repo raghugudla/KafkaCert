95e1e9a7bb5b09916678c945bb1b8c6856b132a2
Raghu Gudla <raghu3786@gmail.com>
2019-03-05 11:01:42 +0100

    adding kafka-streams module

---
+++ kafka-streams/src/main/java/com/github/raghugudla/kafka/streams/tutorial1/WordCountApp.java
@@ -0,0 +1,69 @@
+package com.github.raghugudla.kafka.streams.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class WordCountApp {
+
+    Topology createTopology(){
+        StreamsBuilder builder = new StreamsBuilder();
+        // 1 - stream from Kafka
+
+        KStream<String, String> textLines = builder.stream("word-count-input2");
+        KTable<String, Long> wordCounts = textLines
+                // 2 - map values to lowercase
+                .mapValues((ValueMapper<String, String>) String::toLowerCase)
+                // can be alternatively written as:
+                // .mapValues(String::toLowerCase)
+                // 3 - flatmap values split by space
+                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
+                // 4 - select key to apply a key (we discard the old key)
+                .selectKey((key, word) -> word)
+                // 5 - group by key before aggregation
+                .groupByKey()
+                // 6 - count occurences
+                .count(Materialized.as("Counts2"));
+
+        // 7 - to in order to write the results back to kafka
+        wordCounts.toStream().to("word-count-output2", Produced.with(Serdes.String(), Serdes.Long()));
+
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application2");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        WordCountApp wordCountApp = new WordCountApp();
+
+        KafkaStreams streams = new KafkaStreams(wordCountApp.createTopology(), config);
+        streams.start();
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+        // Update:
+        // print the topology every 10 seconds for learning purposes
+        while(true){
+            streams.localThreadsMetadata().forEach(System.out::println);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+
+    }
+}
